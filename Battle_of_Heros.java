3135b6c8ea58ddfeee3f8ea6413c9b49a5171cb4
Barathi Shankar <barathishankarr@gmail.com>
2025-08-04 08:15:51 +0530

    Create Battle_of_Heros.java
    still under construction

---
+++ Battle_of_Heros.java
@@ -0,0 +1,15 @@
+//Program to create a battle between 2 superheros symbolizing wiht their super powers and attacking each other.The last to survive will be the Champion
+import java.uitl.Scanner;
+import java.util.Random;
+class Heros
+{  String name;
+  int health;
+ int 
+}
+public class Battle_of_Heros
+{
+  public static void main(String[] args)
+  {
+  }
+}
+//STILL UNDER DEVELOPMENT
