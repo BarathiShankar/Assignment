7af9aeb6344f08285bb470d323d03d55ac34190f
Barathi Shankar <barathishankarr@gmail.com>
2025-08-05 12:13:01 +0530

    Developing Battle_of_Heros.java
    developing the structure

--- Battle_of_Heros.java
+++ Battle_of_Heros.java
@@ -4,14 +4,34 @@ import java.util.Random;
 class Heros
 {  String name;
   int health;
- int atk;
+ String user_name;
+ static int matches;
+ Scanner s=new Scanner(System.in);
  Random r=new Random();
  static
- {  System.out.println("--> The game is a battle field of 2 user defined superheros\n--> Each hero has 200 HP at beggining\n--> Each hero attacks the other with his attack strength and reduces opponents HP\n--> On the same time,the attacker gains a small amount of heal\n-->
-  The user has to numerate the hero to be attacked first and give the atatck strenght each time\n--> The game has 3 rounds.Last surviving hero wins the game\n\n\nENJOY THE BATTLE !!!");
+ { matches=0;
+   System.out.println("--> The game is a battle field of 2 user defined superheros\n--> Each hero has 200 HP at beggining\n--> Each hero attacks the other with his attack strength and reduces opponents HP
+     \n--> On the same time,the attacker gains a small amount of heal\n-->
+  The user has to numerate the hero to be attacked first and give the attack strenght each time\n--> The game has 3 rounds.Last surviving hero wins the game\n\n\nENJOY THE BATTLE !!!");
  }
- void attack(Heros opp)
- {
+ void attack(Heros opp)//check dead condition too
+ { int atk=5,heal;
+  System.out.print("Enter the attacking strenght of "+name+" ( 0 to 30 ) : ");
+  atk=s.nextInt();
+  if(atk<31 && atk>=0)
+  {
+    System.out.println("Attacked with a power of "+atk);
+  }
+  else
+    System.out.println("Invalid of attack strenght entered.Default,Attacking set to 5 now");
+  opp.health-=atk;
+  System.out.println("SUPERB BLAST OVER "+opp.name+".HE JUST WOULD HAVE FAINTED !!");
+  heal=r.nextInt(6);
+  if((health+heal <=200))
+  {
+    health+=heal;
+    System.out.println("SURPRISE ! "+name+" (you) Healed with "+heal+" HP's");
+  }
  }
  void stats()
  {
