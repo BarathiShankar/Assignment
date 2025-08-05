98ad146f83888bb734b50760797888ff4ed09c48
Barathi Shankar <barathishankarr@gmail.com>
2025-08-05 08:22:07 +0530

    Update Battle_of_Heros.java
    construction part 1

--- Battle_of_Heros.java
+++ Battle_of_Heros.java
@@ -1,15 +1,33 @@
-//Program to create a battle between 2 superheros symbolizing wiht their super powers and attacking each other.The last to survive will be the Champion
+//Program to create a battle between 2 superheros symbolizing with their super powers and attacking each other.The last to survive will be the Champion
 import java.uitl.Scanner;
 import java.util.Random;
 class Heros
 {  String name;
   int health;
- int 
+ int atk;
+ Random r=new Random();
+ static
+ {  System.out.println("--> The game is a battle field of 2 user defined superheros\n--> Each hero has 200 HP at beggining\n--> Each hero attacks the other with his attack strength and reduces opponents HP\n--> On the same time,the attacker gains a small amount of heal\n-->
+  The user has to numerate the hero to be attacked first and give the atatck strenght each time\n--> The game has 3 rounds.Last surviving hero wins the game\n\n\nENJOY THE BATTLE !!!");
+ }
+ void attack(Heros opp)
+ {
+ }
+ void stats()
+ {
+ }
+ void read()
+ { health=200;
+   
+ }
 }
 public class Battle_of_Heros
 {
   public static void main(String[] args)
-  {
+  { Heros[] H=new Heros[2];
+   Scanner s=new Scanner(System.in);
+   String name;
+   int atk;
   }
 }
 //STILL UNDER DEVELOPMENT
