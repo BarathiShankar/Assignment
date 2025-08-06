ac72b134f2612160029468d6fe628866ac58480a
Barathi Shankar <barathishankarr@gmail.com>
2025-08-06 08:16:56 +0530

    Update Battle_of_Heros.java
    still under update

--- Battle_of_Heros.java
+++ Battle_of_Heros.java
@@ -6,6 +6,7 @@ class Heros
   int health;
  String user_name;
  static int matches;
+ boolean isalive;
  Scanner s=new Scanner(System.in);
  Random r=new Random();
  static
@@ -14,8 +15,11 @@ class Heros
      \n--> On the same time,the attacker gains a small amount of heal\n-->
   The user has to numerate the hero to be attacked first and give the attack strenght each time\n--> The game has 3 rounds.Last surviving hero wins the game\n\n\nENJOY THE BATTLE !!!");
  }
- void attack(Heros opp)//check dead condition too
- { int atk=5,heal;
+ void attack(Heros opp)//check dead condition for attacker,pending
+ { 
+   if(opp.isalive)
+ {
+   int atk=5,heal;
   System.out.print("Enter the attacking strenght of "+name+" ( 0 to 30 ) : ");
   atk=s.nextInt();
   if(atk<31 && atk>=0)
@@ -32,13 +36,26 @@ class Heros
     health+=heal;
     System.out.println("SURPRISE ! "+name+" (you) Healed with "+heal+" HP's");
   }
+   if(opp.health<0)
+   { opp.isalive=false;
+     System.out.println("Congratulations,Your opponent "+opp.name+"is dead !!! You became the CHAMPION !");
+   }
+ }
+  else
+   System.out.println("Congratulations,Your opponent "+opp.name+"is dead !!! You became the CHAMPION !");
  }
  void stats()
  {
+   System.out.println("Details of "+name+" - \n"+"User name : "+user_nmae+"\nHealth available : "+health+"\nIs Alive : "+isalive);
  }
  void read()
  { health=200;
-   
+  isalive=true;
+   System.out.print("Enter the User name : ");
+  user_name=s.next();
+  System.out.print("Choosen hero name : ");
+  name=s.next();
+  System.out.println("FICTIONAL HERO CREATED");
  }
 }
 public class Battle_of_Heros
@@ -48,6 +65,7 @@ public class Battle_of_Heros
    Scanner s=new Scanner(System.in);
    String name;
    int atk;
+   
   }
 }
 //STILL UNDER DEVELOPMENT
