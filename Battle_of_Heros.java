96258cb98483fb9c1f31f0d231fefd7e629c169b
Barathi Shankar <barathishankarr@gmail.com>
2025-08-09 21:27:39 +0530

    Update Battle_of_Heros.java
    updating slowly

--- Battle_of_Heros.java
+++ Battle_of_Heros.java
@@ -4,26 +4,28 @@ import java.util.Random;
 class Heros
 {  String name;
   int health;
+ int strenght;
  String user_name;
  static int matches;
  boolean isalive;
  Scanner s=new Scanner(System.in);
  Random r=new Random();
- static
+ static //To introduce the details and rules of the game to user
  { matches=0;
-   System.out.println("--> The game is a battle field of 2 user defined superheros\n--> Each hero has 200 HP at beggining\n--> Each hero attacks the other with his attack strength and reduces opponents HP
-     \n--> On the same time,the attacker gains a small amount of heal\n-->
-  The user has to numerate the hero to be attacked first and give the attack strenght each time\n--> The game has 3 rounds.Last surviving hero wins the game\n\n\nENJOY THE BATTLE !!!");
+   System.out.println("\n\nTHE RULE PAGE -\n--> The game is a battle field of 2 user defined superheros\n--> Each hero has 200 HP at beggining and 100 strenght power\n--> Each hero attacks the other with his attack power and reduces opponents HP
+     \n--> On the same time,the attacker gains a small amount of heal\n-->The user has to maintain his strength to attack his opponent,lower the strenght makes the attack more weak\n-->
+     With each attack,the attacker looses fraction of his strenght\n-->
+  The user has to numerate the hero to be attacked first and give the attack power each time\n--> The game has 3 rounds.Last surviving hero wins the game\n\n\nENJOY THE BATTLE !!!");
  }
- void attack(Heros opp)//check dead condition for attacker,pending
- { 
+ void attack(Heros opp)//created new rule about attack strenght,managae it
+ { if(isalive)
+ {
    if(opp.isalive)
  {
    int atk=5,heal;
   System.out.print("Enter the attacking strenght of "+name+" ( 0 to 30 ) : ");
   atk=s.nextInt();
-  if(atk<31 && atk>=0)
-  {
+  if(atk<31 && atk>=0)  {
     System.out.println("Attacked with a power of "+atk);
   }
   else
@@ -44,6 +46,9 @@ class Heros
   else
    System.out.println("Congratulations,Your opponent "+opp.name+"is dead !!! You became the CHAMPION !");
  }
+   else
+ System.out.println("You alright my hero .You have ran our of energy ! Declared "+opp.name+" as CHAMPION");
+ }
  void stats()
  {
    System.out.println("Details of "+name+" - \n"+"User name : "+user_nmae+"\nHealth available : "+health+"\nIs Alive : "+isalive);
@@ -57,15 +62,21 @@ class Heros
   name=s.next();
   System.out.println("FICTIONAL HERO CREATED");
  }
-}
 public class Battle_of_Heros
 {
   public static void main(String[] args)
   { Heros[] H=new Heros[2];
    Scanner s=new Scanner(System.in);
-   String name;
+   String name,u_name;
    int atk;
-   
+   for(int i=0;i<2;i++)
+  {
+    System.out.print("Details of Hero "+(i+1)+"\nEnter the user name : ");
+    u_name=s.next();
+    System.out.print("Enter the Hero name : ");
+    name=s.next();
+    System.out.println("Hero created succesfully");
   }
+   
 }
 //STILL UNDER DEVELOPMENT
