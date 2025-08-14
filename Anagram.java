969e9d7464de9dcf4db62571b11d148ccab32137
Barathi Shankar <barathishankarr@gmail.com>
2025-08-14 08:12:49 +0530

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,10 @@
+//Program to check whether 2 strings are anagrams or not
+import java.util.Scanner;
+public class anagram
+  {
+    public static void main(String[]args)
+    {
+      Scanner s=new Scanner(System.in);
+    }
+  }
+//Under develpoment
