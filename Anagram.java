7b7c3e6f9e11a9935fc07d565dddabaf9b795001
Barathi Shankar <barathishankarr@gmail.com>
2025-08-15 10:09:39 +0530

    Update Anagram.java
    Updated input statements

--- Anagram.java
+++ Anagram.java
@@ -5,6 +5,23 @@ public class anagram
     public static void main(String[]args)
     {
       Scanner s=new Scanner(System.in);
+      String str1,str2;
+      int ck=1;
+      while(true)
+        {
+      System.out.print("Enter first string : ");
+      str1=s.next();
+      System.out.print("Enter second string : ");
+          str2=s.next();
+       System.out.print("Enter 0 to exit the program,else enter any other character : ");
+       ck=s.nextInt();
+          if(ck==0)
+          {
+            System.out.println("Program terminated");
+            return,
+          }
+          //do sorting operation and compare the strings
+        }
     }
   }
 //Under develpoment
